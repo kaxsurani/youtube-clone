477533cc7a512f603e5b80f4119f87b89ef70279
Textutils <167845453+kaxsurani@users.noreply.github.com>
2024-09-07 17:11:07 +0530

    Initialize project

---
+++ youtube-clone/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
